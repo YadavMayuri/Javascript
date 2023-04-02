6402ee2fc0ece294ae00d79374e64c2cba1e1c3d
madhuri yadav <mayuriyadav54@gmail.com>
2023-04-02 18:47:27 +0530

    updates

---
+++ server.js
@@ -0,0 +1,53 @@
+// console.log("welcome")
+// var a = 1234
+// console.log(a)
+// console.log(typeof (a))
+// var b = "abcd"
+// console.log(b)
+// console.log(typeof (b))
+// var c = 'ojdhjbd'
+// console.log(c)
+// console.log(typeof (c))
+// var d = `gdhjc`
+// console.log(d)
+// console.log(typeof (d))
+// var e = true
+// console.log(e)
+// console.log(typeof (e))
+// var f = false
+// console.log(f)
+// console.log(typeof (f))
+// var g;
+// console.log(g)
+// var h="4tyj90&&((*"
+// console.log(h)
+// console.log(typeof(h))
+// var a=20;
+// var b=10;
+// console.log(a+b)
+// var addition= a+b
+// console.log(addition)
+// var c=09678
+// var d="hsfbnxba"
+// console.log(c+d)
+// var str= c+d
+// console.log(str)
+// console.log(c===d)
+
+// var c=d
+// console.log(c)
+// var sub= a-b
+// console.log(sub)
+// var check = c===d
+// console.log(check)
+
+// console.log(a=b)
+
+var a;
+var b=1234;
+console.log(a && b)   // if a  is there then only print b
+console.log(a || b)   // print a or b
+ 
+var c = "jxbzx";
+var d = 987;
+console.log(c && d)
