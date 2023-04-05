4e5ba4367db7c1a4a01836fff16cc07ba1688b6b
madhuri yadav <mayuriyadav54@gmail.com>
2023-04-05 11:58:57 +0530

    updates

--- server.js
+++ server.js
@@ -43,11 +43,101 @@
 
 // console.log(a=b)
 
-var a;
-var b=1234;
-console.log(a && b)   // if a  is there then only print b
-console.log(a || b)   // print a or b
- 
-var c = "jxbzx";
-var d = 987;
-console.log(c && d)
+// var a;
+// var b=1234;
+// console.log(a && b)   // if a  is there then only print b
+// console.log(a || b)   // print a or b
+
+// var c = "jxbzx";
+// var d = 987;
+// console.log(c && d)
+
+
+//  conditional statements 
+
+// if-else
+// switch
+
+// if-else statement 
+
+// if(condition){
+//     block of code
+// }
+// else(condition){
+//     block of code
+
+// }
+
+// Q . make driving licence website 
+// 1. allow user for dl if their age is above 18
+// 2. if user age is 18 then allow him for learning licence
+// 3. if user age is below 18 then dont allow for dl
+// 4. if user age is above 80 then dont allow for dl 
+
+// var age = 81;
+
+// if (age >80){
+//     console.log("you are over age")
+// }
+// else if(age ==18){
+//     console.log("you are allowed for learning l")
+// }
+// else if(age >18){
+//     console.log("you are allowed for driving l")
+// }
+// else{
+//     console.log("you are under age")
+// }
+
+// switch statements
+
+// var name = "d";
+// switch (name) {
+//     case "a":
+//         console.log("you are a");
+//         break;
+//     case "b":
+//         console.log("you are b");
+//         break;
+//     case "c":
+//         console.log("you are c");
+//         break;
+//     default:
+//         console.log("name is different from abc")
+// }
+
+
+
+// var age = 18;
+// switch (tru) {
+//     case (age > 80):
+//         console.log("you are over age");
+//         break;
+//     case (age == 18):
+//         console.log("you are allowed for learning license");
+//         break;
+//     case (age > 18):
+//         console.log("you are allowed for driving license");
+//         break;
+//     default:
+//         console.log("you are under age")
+// }
+
+var age = 5;
+switch (true) {
+    case age > 80:
+        console.log("not allow- overage");
+        break;
+    case age == 18:
+        console.log("allow for LL");
+        break;
+    case age > 18:
+        console.log("allow");
+        break;
+    default:
+        console.log("not allow-underage");
+
+}
+
+
+
