ed5646d9c1abd436fe2883ebd1dec73d8811ab84
madhuri yadav <mayuriyadav54@gmail.com>
2023-04-14 12:17:37 +0530

    updates

---
+++ array-2.js
@@ -0,0 +1,57 @@
+// var fruits=["mango","banana","grape","watermelon","orange"];
+// console.log(fruits[0])
+// console.log(fruits[1]);
+// console.log(fruits[2]);
+// console.log(fruits.length);
+
+// for(var i=0; i<fruits.length; i++){
+//     console.log(fruits[i]);
+// }
+
+// var flag = false;
+// for(var i=0; i<fruits.length; i++){
+//     console.log(fruits[i]);
+//     if(fruits[i]=="orange"){
+//         flag=true;
+//     }
+// }
+// console.log("flag is here",flag);
+// if(flag==false){
+//     console.log("orange is not in our array");
+// }else{
+//     console.log("orange is in our array");
+// }
+
+// function findmax(arr){
+//     var max=arr[0];
+//     if(arr[1]>max){
+//         max=arr[1]
+//     }
+//     return max;
+// }
+// var arr=[23994,1000,788999];
+// console.log(findmax(arr));
+
+
+// function findmax(arr){
+//     var max= arr[0];
+//     for(var i=1; i < arr.length; i++){
+//         if(arr[i]> max){
+//             max=arr[i];
+//         }
+//     }
+//     return max;
+// }
+
+// var arr=[23994,1000,788999,8888888855];
+// console.log(findmax(arr));
+
+
+function popup(){
+    alert("This is pop up")
+}
+// popup()
+
+
+
+
